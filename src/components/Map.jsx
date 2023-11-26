db6e0c8fd054476698274ac6fcb2a4e4cf3ca4a6
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-11-26 14:42:10 +0330

    231 - Displaying City Markers on Map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,14 +1,16 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { useState } from "react";
 import styles from "./Map.module.css";
-import {MapContainer, TileLayer , Marker , Popup} from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [mapPosition , setMapPosition] = useState([40 , 0])
+  const { cities } = useCities();
 
+  const [mapPosition, setMapPosition] = useState([40, 0]);
 
   const [searchParams, setSearchParams] = useSearchParams();
 
@@ -16,18 +18,31 @@ function Map() {
   const lng = searchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate('form')}>
-    <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-    />
-    <Marker position={mapPosition}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>
+                <img src={city.emoji}></img>
+              </span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
